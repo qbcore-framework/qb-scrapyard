9327ea2595b3972548e594ee0a2701ece36f7cdf
GhzGarage <qbcore@programmer.net>
2021-04-10 21:55:26 -0500

    Initial Commit

---
+++ config.lua
@@ -0,0 +1,69 @@
+Config = {}
+
+Config.Locations = {
+    [1] = {
+        ["main"] = {x = -254.03, y = 2219.96, z = 8.50},
+        ["deliver"] = {x = -236.41, y = 2239.81, z = 8.67},
+        ["list"] = {x = -232.06, y = 2255.65, z = 8.75},
+        ["search"] = {
+            [1] = {x = -290.04, y = 2225.77, z = 9.11},
+            [2] = {x = -279.43, y = 2248.71, z = 8.59},
+            [3] = {x = -251.54, y = 2258.94, z = 8.60},
+        },
+    }
+}
+
+Config.Items = {
+    "metalscrap",
+    "plastic",
+    "copper",
+    "iron",
+    "aluminum",
+    "steel",
+    "glass",
+}
+
+Config.CurrentVehicles = {}
+
+Config.Vehicles = {
+    [1] = "ninef",
+    [2] = "ninef2",
+    [3] = "banshee",
+    [4] = "alpha",
+    [5] = "baller", 
+    [6] = "bison", 
+    [7] = "huntley", 
+    [8] = "f620", 
+    [9] = "asea", 
+    [10] = "pigalle",
+    [11] = "bullet", 
+    [12] = "turismor", 
+    [13] = "zentorno", 
+    [14] = "dominator",
+    [15] = "blade",
+    [16] = "chino",
+    [17] = "sabregt",
+    [18] = "bati",
+    [19] = "carbonrs",
+    [20] = "akuma",
+    [21] = "thrust",
+    [22] = "exemplar",
+    [23] = "felon",
+    [24] = "sentinel",
+    [25] = "blista",
+    [26] = "fusilade",
+    [27] = "jackal",
+    [28] = "blista2",
+    [29] = "rocoto", 
+    [30] = "seminole", 
+    [31] = "landstalker",
+    [32] = "picador",
+    [33] = "prairie", 
+    [34] = "bobcatxl", 
+    [35] = "gauntlet",
+    [36] = "virgo",
+    [37] = "fq2",
+    [38] = "jester",
+    [39] = "rhapsody",
+    [40] = "feltzer2",
+}
