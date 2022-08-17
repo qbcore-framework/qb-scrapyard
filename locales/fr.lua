9dec066a1841db7d1b5c92c99aa43c46af513291
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-08-17 16:14:29 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,25 +1,25 @@
 local Translations = {
     error = {
-        smash_own = "Vous ne pouvez pas broyer votre propre véhicule.",
-        cannot_scrap = "Ce véhicule ne peut pas être broyer.",
-        not_driver = "Vous n'êtes pas le conducteur",
-        demolish_vehicle = "Vous n'êtes pas autorisé à démolir des véhicules actuellement",
+        smash_own = "Vous ne pouvez pas broyer un véhicule qui vous appartient.",
+        cannot_scrap = "Ce véhicule ne peut pas être broyé.",
+        not_driver = "Vous n'êtes pas le conducteur.",
+        demolish_vehicle = "Vous n'êtes pas autorisé à détruire des véhicules maintenant.",
         canceled = "Annulé",
     },
     text = {
         scrapyard = 'La Casse',
-        disassemble_vehicle = '~g~E~w~ - Démonter le véhicule',
-        email_list = "~g~E~w~ - E-mail Liste des véhicules",
-        demolish_vehicle = "Démolir le véhicule",
+        disassemble_vehicle = '~g~E~w~ - Désassembler le véhicule',
+        email_list = "~g~E~w~ - Recevoir la liste des véhicules",
+        demolish_vehicle = "Déssassemble le véhicule..",
     },
     email = {
         sender = "Turner’s Auto Wrecking",
 		subject = "Liste des véhicules",
-		message = "Vous ne pouvez démolir qu'un certain nombre de véhicules.<br />Vous pouvez garder tout ce que vous démolissez pour toi.<br /><br /><strong>Liste de véhicules :</strong><br />",
+		message = "Vous ne pouvez détruire qu'un certain nombre de véhicules.<br />Vous pouvez garder tout ce que vous détruisez pour vous-même tant que vous ne m'emerdez pas.<br /><br /><strong>Liste des véhicules:</strong><br />",
     },
 }
 
 Lang = Locale:new({
     phrases = Translations,
     warnOnMissing = true
-})
+}) 
