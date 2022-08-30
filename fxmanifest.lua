386cf6fa74262d0749c36ce40346f9e5ae14f8db
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-08-30 17:16:51 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,15 +5,15 @@ description 'QB-Scrapyard'
 version '1.0.0'
 
 shared_script {
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/ComboZone.lua',
     'config.lua',
     '@qb-core/shared/locale.lua',
     'locales/en.lua', -- Change to the language you want
 }
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
     'server/main.lua'
 }
 client_script 'client/main.lua'
