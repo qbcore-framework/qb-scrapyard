a171ff95eaa47b49ddcbac79d2128b2f4bf53aaa
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-02-11 05:59:41 -0600

    Description for txAdmin

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'QB-Scrapyard'
+lua54 'yes'
+author 'Kakarot'
+description 'Allows players to scrap cars for money'
 version '1.2.0'
 
 shared_script {
@@ -13,10 +14,10 @@ shared_script {
     'locales/en.lua',
     'locales/*.lua'
 }
+
+client_script 'client/main.lua'
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
 }
-client_script 'client/main.lua'
-
-lua54 'yes'
