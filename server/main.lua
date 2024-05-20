7dd0e3e3eb52b8ff5d560894225f173637a2d937
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-05-20 05:31:14 -0500

    Inventory Update

--- server/main.lua
+++ server/main.lua
@@ -10,12 +10,12 @@ end)
 
 RegisterNetEvent('qb-scrapyard:server:LoadVehicleList', function()
     local src = source
-    TriggerClientEvent("qb-scapyard:client:setNewVehicles", src, Config.CurrentVehicles)
+    TriggerClientEvent('qb-scapyard:client:setNewVehicles', src, Config.CurrentVehicles)
 end)
 
 
 QBCore.Functions.CreateCallback('qb-scrapyard:checkOwnerVehicle', function(_, cb, plate)
-    local result = MySQL.scalar.await("SELECT `plate` FROM `player_vehicles` WHERE `plate` = ?",{plate})
+    local result = MySQL.scalar.await('SELECT `plate` FROM `player_vehicles` WHERE `plate` = ?', { plate })
     if result then
         cb(false)
     else
@@ -30,20 +30,19 @@ RegisterNetEvent('qb-scrapyard:server:ScrapVehicle', function(listKey)
     if Config.CurrentVehicles[listKey] ~= nil then
         for _ = 1, math.random(2, 4), 1 do
             local item = Config.Items[math.random(1, #Config.Items)]
-            Player.Functions.AddItem(item, math.random(25, 45))
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], 'add')
+            exports['qb-inventory']:AddItem(src, item, math.random(25, 45), false, false, 'qb-scrapyard:server:ScrapVehicle')
+            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[item], 'add')
             Wait(500)
         end
         local Luck = math.random(1, 8)
         local Odd = math.random(1, 8)
         if Luck == Odd then
             local random = math.random(10, 20)
-            Player.Functions.AddItem("rubber", random)
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["rubber"], 'add')
-
+            exports['qb-inventory']:AddItem(src, 'rubber', random, false, false, 'qb-scrapyard:server:ScrapVehicle')
+            TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items['rubber'], 'add')
         end
         Config.CurrentVehicles[listKey] = nil
-        TriggerClientEvent("qb-scapyard:client:setNewVehicles", -1, Config.CurrentVehicles)
+        TriggerClientEvent('qb-scapyard:client:setNewVehicles', -1, Config.CurrentVehicles)
     end
 end)
 
@@ -55,7 +54,7 @@ function GenerateVehicleList()
             Config.CurrentVehicles[i] = randVehicle
         end
     end
-    TriggerClientEvent("qb-scapyard:client:setNewVehicles", -1, Config.CurrentVehicles)
+    TriggerClientEvent('qb-scapyard:client:setNewVehicles', -1, Config.CurrentVehicles)
 end
 
 function IsInList(name)
